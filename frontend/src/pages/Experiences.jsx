97e15aae1425aa0e932dee87834395ae11ca0732
Anuda Ransara <anudaransara1@gmail.com>
2026-05-13 12:53:25 +0530

    create expeiace page heder.

--- frontend/src/pages/Experiences.jsx
+++ frontend/src/pages/Experiences.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HederExperiences from '../components/HederExperiences'
 
 function Experiences() {
   return (
     <div>
-        Experiences
+        <HederExperiences/>
     </div>
   )
 }
